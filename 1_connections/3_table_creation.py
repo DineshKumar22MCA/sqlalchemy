5d82ddf27bda20569c040f869a8390ca0fb5b771
DineshKumar22MCA <dinesh1822.cbcs@gmail.com>
2025-03-18 18:12:54 +0530

    sqlalchemy connections added

---
+++ 1_connections/3_table_creation.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, String, Integer, text
+from sqlalchemy.orm import declarative_base
+
+from connection import engine
+from orm_connection import tables
+
+
+try:
+    Base = declarative_base()
+
+
+    class User(Base):
+        __tablename__ = "user"
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String(255))
+
+    Base.metadata.create_all(engine)
+
+    #check tables
+    if "user" in tables:
+        print("table created successfully")
+    else:
+        print("table is not created")
+    
+    with engine.connect() as con:
+        result = con.execute(text("show tables like 'user'"))
+        table_exists = result.fetchone() is not None
+        if table_exists:
+            print("table created successfully")
+        else:
+            print("table is not created")
+
+    if 'user' in Base.metadata.tables:
+        print("table created")
+    else:
+        print("table not created")
+except Exception as e:
+    print(str(e))
